c2928d2b496fe50be2851e68c8444e4ee3f77894
Your Name <you@example.com>
2023-10-14 17:23:46 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const timeEl = document.querySelector('#time')
+const dateEl = document.querySelector('#date')
+const currentWeatherItemEl = document.querySelector('#current-weather-items')
+const timeZone = document.querySelector('#time-zone')
+const countryEl = document.querySelector('#country')
+const weatherForecastEl = document.querySelector('#weather-forecast')
+const currentTempEl = document.querySelector('#current-temp')
+
+const API_KEY = "3c780ff6824fb40662843628e2d06723"
+
+
